874fa0d390c6043c97f4c5f1306f87d90ef785af
CoKayne <duankayne@gmail.com>
2024-01-18 14:44:48 +0800

    remove parse_test.py for simplicity

--- tests/parse_test.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-resp = requests.get('https://www.youtube.com/onepiece')
-soup = BeautifulSoup(resp.text, 'html.parser')
-
-channel_id = soup.select_one('meta[property="og:url"]')['content'].strip('/').split('/')[-1]
-print(channel_id)
